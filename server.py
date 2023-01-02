144d3d42450ae26fa0441aeb0c905b97c192b42d
Berathan Yedibela <73439036+Ber4tBey@users.noreply.github.com>
2023-01-02 17:52:10 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,8 +2,26 @@ from flask import Flask
 server = Flask(__name__)
 
 @server.route("/")
-def hello():
-    return "Hello World!"
+def index():
+
+ return "Lavander Api Hoşgeldiniz"
+ 
+
+@server.route("/haberler")
+def indexx():
+
+ return scrapheaal()
+
+@server.route("/enyuksek")
+def indexxx():
+
+ return request(filter_start(),filter_end())
+
+
+@server.route("/depremler")
+def depremler():
+    r = get("https://api.orhanaydogdu.com.tr/deprem/live.php?limit=100") # orhanaydogdu special thx 
+    return json.loads(r.content)
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0', port=1337)
